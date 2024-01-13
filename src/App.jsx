4c12867b8c493b25ca372bc2f9ee9fb912060b6a
Voslo <rewqvolty228@gmail.com>
2024-01-13 21:47:51 +0200

    2324

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const Card = ({ children }) => {
+  return <div>{children}</div>;
+};
+
+export default function App() {
+   <Card>
+    <h1>Card title</h1>
+    <p>Text between opening and closing tag</p>
+  </Card>
+}
