d81e6901f51fca504a3fcb0b9ec6a16063123f7c
Voslo <rewqvolty228@gmail.com>
2024-01-15 18:56:29 +0200

    23

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,9 +1,16 @@
-const FriendListItem = ({ avatar, name, isOnline}) => {
+import clsx from "clsx";
+import css from "./FriendList.module.css";
+
+
+const FriendListItem = ({ avatar, name, isOnline }) => {
     return (
-        <div className="item">
+        <div className={css.item}>
             <img className="avatar" src={avatar} alt="Avatar" width="48" />
             <p className="name">{name}</p>
-            <p className={`status ${isOnline ? 'online' : 'offline'}`}>{isOnline ? 'Online' : 'Offline'}</p>
+            <p className={clsx(css.status, { [css.isOnline]: isOnline, [css.isOffline]: !isOnline })}>
+                {isOnline ? 'Online' : 'Offline'}
+            </p>
+
         </div>
     )
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,27 +1,30 @@
-const Profile = ({ name, tag, location, image, stats}) => {
+import css from "./Profile.module.css";
+
+const Profile = ({ name, tag, location, image, stats }) => {
 
     return (
-        <div className="profile">
+        <div className={css.profile}>
             <div className="description">
                 <img
                     src={image}
                     alt="User avatar"
                     className="avatar"
+                    width="200px"
                 />
                 <p className="name">{name}</p>
                 <p className="tag">@{tag}</p>
                 <p className="location">{location}</p>
             </div>
-            <ul className="stats">
-                <li>
+            <ul className={css.stats}>
+                <li className={css.statsItem}>
                     <span className="label">Followers</span>
                     <span className="value">{stats.followers}</span>
                 </li>
-                <li>
+                <li className={css.statsItem}>
                     <span className="label">Views</span>
                     <span className="value">{stats.views}</span>
                 </li>
-                <li>
+                <li className={css.statsItem}>
                     <span className="label">Likes</span>
                     <span className="value">{stats.likes}</span>
                 </li>
